8c0e8163fbb15cba52b928b2510ffdacaaa3dca3
Adam Selker <adam.selker@joby.aero>
2021-04-24 01:19:58 -0700

    I've screwed it up already, haven't I

---
+++ ui_sandbox.py
@@ -0,0 +1,50 @@
+from blessed import Terminal
+
+
+class NanoEditor:
+    def __init__(self, n_lines=32, n_cols=40):
+        self.cursor = (0, 0)
+        self.contents = [[]]
+
+
+box_names = [
+    ["Puzzle 1", "Puzzle 2", "Puzzle 3", "Puzzle 4"],
+    ["Asm 1", "Asm 2", "Asm 3", "Asm 4"],
+    ["Ucode 1", "Ucode 2", "Ucode 3", "Ucode 4"],
+]
+
+term = Terminal()
+
+while term.width < 120:
+    print(term.home + term.clear)
+    print(term.white_on_black("Please resize terminal to at least 120 columns."))
+    _ = term.inkey(timeout=0.1)  # Probably better way to do this?
+while term.height < 40:
+    print(term.home + term.clear)
+    print(term.white_on_black("Please resize terminal to at least 40 lines."))
+    _ = term.inkey(timeout=0.1)  # Probably better way to do this?
+
+print(term.home + term.clear)
+
+"""
+with term.cbreak(), term.hidden_cursor():
+    inp = term.inkey()
+
+print(term.move_down(2) + "You pressed " + term.bold(repr(inp)))
+"""
+
+assert not any([any([8 < len(name) for name in names]) for names in box_names])
+
+for y, names in enumerate(box_names):
+    for x, name in enumerate(names):
+        is_highlighted = x == 1 and y == 1
+        box_color = term.black_on_white if is_highlighted else term.white_on_black
+        print(term.move_xy(10 + 30*x, 5 + 13*y))
+        print(box_color(" "*10))
+        print(term.move_xy(10 + 30*x, 6 + 13*y))
+        print(box_color(" ") + term.white_on_black(name))
+        print(term.move_xy(18 + 30*x, 6 + 13*y))
+        print(box_color(" "))
+        print(term.move_xy(10 + 30*x, 7 + 13*y))
+        print(box_color(" "*10))
+

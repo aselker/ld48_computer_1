10bb24d188ee70a8e6397766eee99d2fd076baba
Adam Selker <adam.selker@joby.aero>
2021-04-24 11:58:35 -0700

    Two-state ui, ayy

--- ui_sandbox.py
+++ ui_sandbox.py
@@ -1,17 +1,69 @@
 from blessed import Terminal
 
 
+class OverviewScreen:
+    """
+    TODO: What is this actually for?
+    """
+
+    def __init__(self):
+
+        self.box_names = [
+            ["Puzzle 1", "Puzzle 2", "Puzzle 3", "Puzzle 4"],
+            ["Asm 1", "Asm 2", "Asm 3", "Asm 4"],
+            ["Ucode 1", "Ucode 2", "Ucode 3", "Ucode 4"],
+        ]
+
+        assert not any([any([8 < len(name) for name in names]) for names in self.box_names])
+        self.cursor = [0, 0]
+
+        self.draw()
+
+    def draw(self):
+        # TODO: Overwrite the whole screen
+        for y, names in enumerate(self.box_names):
+            for x, name in enumerate(names):
+                is_highlighted = [x, y] == self.cursor
+                box_color = term.black_on_white if is_highlighted else term.white_on_black
+                print(term.move_xy(10 + 30 * x, 5 + 13 * y) + box_color(" " * 10))
+                print(term.move_xy(10 + 30 * x, 6 + 13 * y) + box_color(" ") + term.white_on_black(name))
+                print(term.move_xy(19 + 30 * x, 6 + 13 * y) + box_color(" "))
+                print(term.move_xy(10 + 30 * x, 7 + 13 * y) + box_color(" " * 10))
+
+    def keypress(self, inp):
+        if inp.code == term.KEY_LEFT:
+            if 0 < self.cursor[0]:
+                self.cursor[0] -= 1
+        elif inp.code == term.KEY_RIGHT:
+            if self.cursor[0] < 3:
+                self.cursor[0] += 1
+        elif inp.code == term.KEY_UP:
+            if 0 < self.cursor[1]:
+                self.cursor[1] -= 1
+        elif inp.code == term.KEY_DOWN:
+            if self.cursor[1] < 2:
+                self.cursor[1] += 1
+
+        self.draw()
+
+
 class NanoEditor:
-    def __init__(self, origin, size):
+    def __init__(self, origin, size, contents=None):
         self.origin = origin
         self.size = size
         self.cursor = [0, 0]
-        self.contents = [[] for _ in range(self.size[1])]
+
+        if contents is None:
+            self.contents = [[] for _ in range(self.size[1])]
+        else:
+            self.contents = contents
+
         self.legal_chars = (
             [chr(d) for d in range(ord("0"), ord("9") + 1)]
             + [chr(c) for c in range(ord("A"), ord("Z") + 1)]
             + list(" =!@#$%^&*().")
         )
+
         self.draw()
 
     def draw(self):
@@ -64,12 +116,6 @@ class NanoEditor:
         self.draw()
 
 
-box_names = [
-    ["Puzzle 1", "Puzzle 2", "Puzzle 3", "Puzzle 4"],
-    ["Asm 1", "Asm 2", "Asm 3", "Asm 4"],
-    ["Ucode 1", "Ucode 2", "Ucode 3", "Ucode 4"],
-]
-
 term = Terminal()
 
 while term.width < 120:
@@ -83,46 +129,32 @@ while term.height < 40:
 
 print(term.home + term.clear)
 
-"""
-with term.cbreak(), term.hidden_cursor():
-    inp = term.inkey()
 
-print(term.move_down(2) + term.move_right(20))
-print("You pressed " + term.bold(repr(inp)))
-"""
-
-assert not any([any([8 < len(name) for name in names]) for names in box_names])
-cursor = [0, 0]
-
-while True:
-    for y, names in enumerate(box_names):
-        for x, name in enumerate(names):
-            is_highlighted = [x, y] == cursor
-            box_color = term.black_on_white if is_highlighted else term.white_on_black
-            print(term.move_xy(10 + 30 * x, 5 + 13 * y) + box_color(" " * 10))
-            print(term.move_xy(10 + 30 * x, 6 + 13 * y) + box_color(" ") + term.white_on_black(name))
-            print(term.move_xy(19 + 30 * x, 6 + 13 * y) + box_color(" "))
-            print(term.move_xy(10 + 30 * x, 7 + 13 * y) + box_color(" " * 10))
-
-    with term.cbreak(), term.hidden_cursor():
-        inp = term.inkey()
-    if inp.code == term.KEY_LEFT:
-        if 0 < cursor[0]:
-            cursor[0] -= 1
-    elif inp.code == term.KEY_RIGHT:
-        if cursor[0] < 3:
-            cursor[0] += 1
-    elif inp.code == term.KEY_UP:
-        if 0 < cursor[1]:
-            cursor[1] -= 1
-    elif inp.code == term.KEY_DOWN:
-        if cursor[1] < 2:
-            cursor[1] += 1
-    elif inp.code == term.KEY_ESCAPE:
-        break
-    elif inp.code == term.KEY_ENTER:
-        editor = NanoEditor((5, 2), (24, 32))
+class Screen:
+    def __init__(self):
+        self.go_to_state(("overview", 0))
         while True:
             with term.cbreak(), term.hidden_cursor():
                 inp = term.inkey()
-            editor.keypress(inp)
+            if inp.code == term.KEY_ESCAPE:
+                if self.state[0] == "overview":
+                    break
+                elif self.state[0] == "editor":
+                    self.go_to_state(("overview", 0))
+            if inp.code == term.KEY_ENTER:
+                if self.state[0] == "overview":
+                    self.go_to_state(("editor", 0))
+                elif self.state[0] == "editor":
+                    pass
+            else:
+                self.screen.keypress(inp)
+
+    def go_to_state(self, new_state):
+        self.state = new_state
+        if self.state[0] == "overview":
+            self.screen = OverviewScreen()
+        elif self.state[0] == "editor":
+            self.screen = NanoEditor((5, 2), (24, 32))
+
+
+Screen()

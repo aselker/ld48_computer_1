ab1680d9a3b04c5288b507014694b7339acc3963
Adam Selker <adam.selker@joby.aero>
2021-04-24 01:30:20 -0700

    This is literally the most advanced non-text UI I have built

--- ui_sandbox.py
+++ ui_sandbox.py
@@ -30,21 +30,38 @@ print(term.home + term.clear)
 with term.cbreak(), term.hidden_cursor():
     inp = term.inkey()
 
-print(term.move_down(2) + "You pressed " + term.bold(repr(inp)))
+print(term.move_down(2) + term.move_right(20))
+print("You pressed " + term.bold(repr(inp)))
 """
 
 assert not any([any([8 < len(name) for name in names]) for names in box_names])
+selection = [0, 0]
 
-for y, names in enumerate(box_names):
-    for x, name in enumerate(names):
-        is_highlighted = x == 1 and y == 1
-        box_color = term.black_on_white if is_highlighted else term.white_on_black
-        print(term.move_xy(10 + 30*x, 5 + 13*y))
-        print(box_color(" "*10))
-        print(term.move_xy(10 + 30*x, 6 + 13*y))
-        print(box_color(" ") + term.white_on_black(name))
-        print(term.move_xy(18 + 30*x, 6 + 13*y))
-        print(box_color(" "))
-        print(term.move_xy(10 + 30*x, 7 + 13*y))
-        print(box_color(" "*10))
+while True:
+    for y, names in enumerate(box_names):
+        for x, name in enumerate(names):
+            is_highlighted = [x,y] == selection
+            box_color = term.black_on_white if is_highlighted else term.white_on_black
+            print(term.move_xy(10 + 30 * x, 5 + 13 * y) + box_color(" " * 10))
+            print(term.move_xy(10 + 30 * x, 6 + 13 * y) + box_color(" ") + term.white_on_black(name))
+            print(term.move_xy(19 + 30 * x, 6 + 13 * y) + box_color(" "))
+            print(term.move_xy(10 + 30 * x, 7 + 13 * y) + box_color(" " * 10))
+
+    with term.cbreak(), term.hidden_cursor():
+        inp = term.inkey()
+    if inp.code == term.KEY_LEFT:
+        if 0 < selection[0]:
+            selection[0] -= 1
+    elif inp.code == term.KEY_RIGHT:
+        if selection[0] < 3:
+            selection[0] += 1
+    elif inp.code == term.KEY_UP:
+        if 0 < selection[1]:
+            selection[1] -= 1
+    elif inp.code == term.KEY_DOWN:
+        if selection[1] < 2:
+            selection[1] += 1
+    elif inp.code == term.KEY_ESCAPE:
+        break
+
 

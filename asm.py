8c21263b624530e52105017fb6d95cb8f49f4951
Adam Selker <adam.selker@joby.aero>
2021-04-24 19:17:06 -0700

    Add asm

---
+++ asm.py
@@ -0,0 +1,133 @@
+def cmd_add(asm):
+    print("Sum:", asm.pipe1 + asm.pipe2)
+    return asm.pipe1 + asm.pipe2, None
+
+
+def cmd_jmp(asm):
+    return 0, asm.pipe1
+
+
+def cmd_push(asm):
+    asm.stack.append(asm.pipe1)
+    return 0, None
+
+
+def cmd_pop(asm):
+    if len(asm.stack) == 0:
+        return 0, none
+    return asm.stack.pop(), None
+
+
+commands = {
+    "add": cmd_add,
+    "jmp": cmd_jmp,
+    "push": cmd_push,
+    "pop": cmd_pop,
+}
+
+
+def make_literal_fn(literal):
+    def fn(asm):
+        return literal, None
+
+    return fn
+
+
+class Asm:
+    @classmethod
+    def parse(cls, lines):
+        cmds = []
+        for line in lines:
+            if type(line) is list:
+                line = "".join(line)
+
+            line = line.rstrip()
+
+            # Blank line -> blank cmd
+            if line == "":
+                cmds.append([])
+                continue
+
+            subcmds = line.lower().split("|")
+
+            ok = True
+            new_subcmds = []
+            for subcmd in subcmds:
+                words = subcmd.strip().split(" ")
+                if len(words) not in [1, 2]:
+                    ok = False
+                    break
+
+                # Validate each word
+                ok2 = True
+                new_words = []
+                for word in words:
+                    if word.isdigit():
+                        literal = int(word)
+                        if 0 <= literal and literal < 2 ** 6:
+                            new_words.append(make_literal_fn(literal))
+                        else:
+                            ok2 = False
+                            break
+                    elif word not in commands:
+                        ok2 = False
+                        break
+                    else:
+                        new_words.append(commands[word])
+                if not ok2:
+                    ok = False
+                    break
+
+                new_subcmds.append(new_words)
+
+            if ok:
+                cmds.append(new_subcmds)
+            else:
+                cmds.append(None)
+
+        return cmds
+
+    def __init__(self, cmds, ucodes):
+        self.cmds = cmds
+        self.ucodes = ucodes
+
+        self.stack = []
+        self.pc = 0
+
+    def step(self):
+        cmd = self.cmds[self.pc]
+        self.pipe1 = 0
+        self.pipe2 = 0
+        next_pc = self.pc + 1
+
+        for subcmd in cmd:
+            next_pipe1, jump_addr_1 = subcmd[0](self)
+            if len(subcmd) == 2:
+                next_pipe2, jump_addr_2 = subcmd[1](self)
+            else:
+                next_pipe2, jump_addr_2 = 0, None
+
+            self.pipe1 = next_pipe1
+            self.pipe2 = next_pipe2
+
+            if jump_addr_1 is not None:
+                next_pc = jump_addr_1
+
+            if jump_addr_2 is not None:
+                next_pc = jump_addr_2
+
+        self.pc = next_pc
+
+
+    def run(self):
+        while self.pc < len(self.cmds):
+            self.step()
+
+
+if __name__ == "__main__":
+    cmds = Asm.parse(["1 1 | add | push", "pop 2 | add | jmp", "3 | push", "5 | push",])
+    print("Cmds:", cmds)
+
+    asm = Asm(cmds, [])
+    asm.run()
+    print("Stack:", asm.stack)

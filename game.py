0cd10a1d895305f38abe9c67937c6ccbc2c2ceae
Adam Selker <adam.selker@joby.aero>
2021-04-25 17:56:23 -0700

    42 lines, not 40, for buffer

--- game.py
+++ game.py
@@ -13,14 +13,14 @@ print(term.home + term.clear)
 editor = PuzzleList(term, esc_delay)
 editor.draw()
 while True:
-    while term.width < 120 or term.height < 40:
+    while term.width < 120 or term.height < 42:
         while term.width < 120:
             print(term.home + term.clear)
             print(term.white_on_black("Please resize terminal to at least 120 columns."))
             _ = term.inkey(timeout=0.1)  # Probably better way to do this?
-        while term.height < 40:
+        while term.height < 42:
             print(term.home + term.clear)
-            print(term.white_on_black("Please resize terminal to at least 40 lines."))
+            print(term.white_on_black("Please resize terminal to at least 42 lines."))
             _ = term.inkey(timeout=0.1)  # Probably better way to do this?
         editor.draw()
     with term.cbreak(), term.hidden_cursor():
